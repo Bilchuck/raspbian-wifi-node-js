a9e83d9da1a91072ca0fe10389aa9b602ee1f568
anton.bilchuk <anton.bilchuk@stylelounge.com>
2017-11-28 16:37:36 +0200

    feat: refactor connectToWifi function

--- src/reconfigWifiSettings.js
+++ src/reconfigWifiSettings.js
@@ -1,7 +1,14 @@
 const cmd = require("node-cmd");
 
-cmd.get("wpa_cli -i wlan0 reconfigure", (err, str) => {
-    if (err) {
-        throw err;
-    }
-});
+const reconfigWifiSettings = () => {
+    console.log(`Reconfiguring wifi..`);
+    cmd.get("wpa_cli -i wlan0 reconfigure", (err, str) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        console.log(`Reconfigured!`)
+    });
+};
+
+module.exports = reconfigWifiSettings;
