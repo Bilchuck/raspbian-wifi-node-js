dfea87e8d7ab9cfc4e2b5804edf830a871064626
anton.bilchuk <anton.bilchuk@stylelounge.com>
2017-11-28 16:12:55 +0200

    feat: fix parse function

--- src/parseIwlistOutput.js
+++ src/parseIwlistOutput.js
@@ -1,20 +1,24 @@
 const _ = require("ramda");
 
-const parseIwlistOutput = output => _.compose(
-    _.map(buildWifiDTO),
-    _.reduce(byCeils),
-    _.tail,
-    _.filter(length),
-    _.split("\n"),
-)
-const isCeail = rl => el.includes("Cell");
-const byCeils = (acc, row) => isCeail(row) ? [...acc, []] : _.over(_.last, _.push(row), acc);
+const isCeail = el => el.includes("Cell");
+const isEssid = str => str.includes("ESSID");
+const isWpa = arr => arr.some(str => str.includes("WPA"));
+
+const byCeils = (acc, row) => isCeail(row) ? [...acc, []] : _.over(_.lensIndex(-1), arr => [...arr, row], acc);
+
+const parseEssidLine = line => line.replace(/"/gi, "").replace(/ESSID:/gi, "").trim();
 
 const buildWifiDTO = lines => ({
-    essid: _.compose(parseEssidLine, first, filter(isEssid))(lines),
+    essid: _.compose(parseEssidLine, _.head, _.filter(isEssid))(lines),
     encryption: isWpa(lines) ? "WPA-PSK" : "NONE",
 });
 
-const parseEssidLine = line => line.replace(/"/gi, "").replace(/ESSID:/gi, "").trim();
+const parseIwlistOutput = _.compose(
+    _.map(buildWifiDTO),
+    _.reduce(byCeils, []),
+    _.tail,
+    _.filter(_.length),
+    _.split("\n"),
+);
 
 module.exports = parseIwlistOutput;
